251f318431b99a366442c1935e55976ac16c2878
MiguelDev5 <migangmedina@gmail.com>
2024-09-04 17:27:24 -0500

    Add tyt_recruitment extension

---
+++ tyt_recruitment/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Reclutamiento TYT Contact Center - Soy Calidad',
+    'version': '1.0',
+    'description': 'Añade características al módulo Reclutamiento TYT Contact Center',
+    'summary': 'Añade características al módulo Reclutamiento TYT Contact Center',
+    'author': 'Soy Calidad',
+    'website': 'www.soycalidad.com',
+    'license': '',
+    'category': 'soycalidad',
+    'depends': ['hr_recruitment'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'reports/reports_xls.xml',
+    ],
+    'auto_install': False,
+    'installable': True,
+    'application': False,
+}
