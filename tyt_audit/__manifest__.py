5e539e245769c7c923668bd0dca474deb9611380
Paul Chipana <paulhackingdeveloper@gmail.com>
2024-09-23 17:14:33 -0500

    add tyt_audit report

--- tyt_audit/__manifest__.py
+++ tyt_audit/__manifest__.py
@@ -8,13 +8,22 @@
     'license': '',
     'category': 'soycalidad',
     'depends': [
+        'mgmtsystem_nonconformity',
         'mgmtsystem_audit',
-        'mgmtsystem_nonconformity.xml',
+        
     ],
     'data': [
         #'security/ir.model.access.csv',
         'views/audit.xml',
+        'reports/report_paperformat.xml',               
+        'reports/report_layout.xml',        
+        'reports/report_informe.xml',
     ],
+    'assets': {
+        'web.report_assets_common': [
+                "/tyt_audit/static/src/scss/report_informe.scss",
+        ],
+    },
     'auto_install': False,
     'installable': True,
     'application': False,
--- tyt_audit/models/__init__.py
+++ tyt_audit/models/__init__.py
@@ -1,3 +1,2 @@
 # -*- coding: utf-8 -*-
 #from . import res_users
-from . import context
