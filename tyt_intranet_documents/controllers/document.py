9266b87d1ac59824c866697b32df066d061158bb
SoyCalidad <soycalidad.20@gmail.com>
2024-11-06 13:16:05 -0500

    update intranet documents

--- tyt_intranet_documents/controllers/document.py
+++ tyt_intranet_documents/controllers/document.py
@@ -17,8 +17,6 @@ from odoo.osv import expression
 from odoo.tools import format_datetime, format_date, is_html_empty
 from odoo.addons.base.models.ir_qweb import keep_query
 
-_logger = logging.getLogger(__name__)
-
 
 class DocumentController(http.Controller):
 
@@ -28,12 +26,23 @@ class DocumentController(http.Controller):
             all_documents |= self.get_all_documents(child_folder)
         return all_documents
 
+    @api.model
+    def _check_folder_access(self, folder):
+        return folder.is_intranet_folder and folder.read_group_ids & request.env.user.groups_id
+
+    @api.model
+    def _check_document_access(self, document):
+        if document.folder_id:
+            return self._check_folder_access(document.folder_id)
+        return False
+
     @http.route('/documents/folder/share', type='http', auth='user', website=True)
     def documents_folder_share(self):
         values = request.params.copy()
         folders = request.env['documents.folder'].sudo().search([('is_intranet_folder', '=', True)])
+        accessible_folders = folders.filtered(lambda folder: self._check_folder_access(folder))
         values.update({
-            'folders': folders,
+            'folders': accessible_folders,
         })
         return request.render('tyt_intranet_documents.portal_documents_folder_share', values)
 
@@ -41,9 +50,8 @@ class DocumentController(http.Controller):
     def my_documents_share(self, folder_id):
         values = request.params.copy()
         folder = request.env['documents.folder'].sudo().browse(folder_id)
-        if not folder.exists():
+        if not folder.exists() or not self._check_folder_access(folder):
             return request.redirect('/my')
-
         documents = folder.document_ids
         document_count = len(documents)
         values.update({
@@ -55,24 +63,12 @@ class DocumentController(http.Controller):
 
     @http.route('/my/document/view/<int:document_id>', type='http', auth='user', website=True)
     def my_document_view(self, document_id):
-        values = request.params.copy()
         document = request.env['documents.document'].sudo().browse(document_id)
+        if not document.exists() or not self._check_document_access(document):
+            return request.redirect('/my')
         values = {
             'page_name': 'document',
             'document': document,
             'url': f'/web/content/{document.id}?model%3Ddocuments.document',
         }
-
         return request.render('tyt_intranet_documents.portal_my_document_view', values)
-
-    @http.route('/my/document/share/<int:document_id>', type='http', auth='user', website=True)
-    def mailbox_form_share(self, document_id):
-        values = request.params.copy()
-        document = request.env['documents.document'].sudo().browse(document_id)
-        values = {
-            'page_name': 'document',
-            'document': document,
-            'url': '/view/document/' + str(document.id),
-        }
-
-        return request.render('tyt_intranet_documents.portal_my_document_share_form', values)
