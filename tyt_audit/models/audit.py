dbb73ba7d1a3fb2a17fe04100c35758f53e4191b
Paul Chipana <paulhackingdeveloper@gmail.com>
2024-11-06 11:34:34 -0500

    botón "Generar Auditoría"

--- tyt_audit/models/audit.py
+++ tyt_audit/models/audit.py
@@ -5,36 +5,38 @@ from datetime import datetime
 from odoo import _, api, fields, models, tools
 from odoo.exceptions import ValidationError, Warning
 
+
 class AuditProcedure(models.Model):
     _name = "audit.audit.procedure"
     _description = "Procedimiento de Lista de Verificación"
 
     name = fields.Char(
-        string='Nombre',
+        string="Nombre",
         required=True,
     )
+
 
 class AuditPlanningClause(models.Model):
     _name = "audit.audit.planning.iso9001_standard"
     _description = "Lista de Verificación / Planificación / Cláusula"
 
     name = fields.Char(
-        string='Nombre',
+        string="Nombre",
         required=True,
     )
 
     complete_name = fields.Char(
-        string='Nombre',
+        string="Nombre",
         required=True,
     )
 
     combined_name = fields.Char(
-        string='Nombre Combinado',
-        compute='_compute_combined_name',
+        string="Nombre Combinado",
+        compute="_compute_combined_name",
         store=True,  # Opcional: almacena el valor en la base de datos
-    )    
+    )
 
-    @api.depends('name', 'complete_name')
+    @api.depends("name", "complete_name")
     def _compute_combined_name(self):
         for record in self:
             if record.name and record.complete_name:
@@ -42,21 +44,23 @@ class AuditPlanningClause(models.Model):
             else:
                 record.combined_name = record.name or record.complete_name
 
+
 class AuditPlanningClause(models.Model):
     _name = "audit.audit.planning.clause"
     _description = "Lista de Verificación / Planificación / Cláusula"
 
     name = fields.Char(
-        string='Nombre',
+        string="Nombre",
         required=True,
     )
+
 
 class AuditPlanningEvidence(models.Model):
     _name = "audit.audit.planning.evidence"
     _description = "Lista de Verificación / Planificación / Evidencia"
 
     name = fields.Char(
-        string='Nombre',
+        string="Nombre",
         required=True,
     )
 
@@ -65,175 +69,179 @@ class AuditPlanning(models.Model):
     _name = "audit.audit.planning"
     _description = "Lista de Verificación / Planificación"
 
-    name = fields.Char(
-        string='Nombre'
-    )
+    name = fields.Char(string="Nombre")
 
     audit_audit_id = fields.Many2one(
-        'audit.audit',
-        string="Lista de verificación",
-        store=True
+        "audit.audit", string="Lista de verificación", store=True
     )
 
     audit_report_id = fields.Many2one(
-        'audit.report',
-        string="Informe de Auditoría",
-        store=True
+        "audit.report", string="Informe de Auditoría", store=True
     )
 
     iso_9001_standards_ids = fields.Many2many(
-        'audit.audit.planning.iso9001_standard',
-        string='Norma ISO 9001:2015'
+        "audit.audit.planning.iso9001_standard", string="Norma ISO 9001:2015"
     )
 
     clause_id = fields.Many2one(
-        string='Cláusula',
-        comodel_name='audit.audit.planning.clause'
+        string="Cláusula", comodel_name="audit.audit.planning.clause"
     )
 
     employee_id = fields.Many2one(
-        string='Responsable',
-        comodel_name='hr.employee',
+        string="Responsable",
+        comodel_name="hr.employee",
     )
 
     employee_job_id = fields.Many2one(
-        comodel_name='hr.job',
-        string='Puesto de Responsable',
-        related='employee_id.job_id',
+        comodel_name="hr.job",
+        string="Puesto de Responsable",
+        related="employee_id.job_id",
         store=True,
         readonly=True,
     )
 
-    verification = fields.Char(string='Verificación')
+    verification = fields.Char(string="Verificación")
 
     finding = fields.Selection(
         selection=[
             ("non_conformity", "No Conformidad"),
-            ("good_practices", "Buenas Prácticas")
+            ("good_practices", "Buenas Prácticas"),
         ],
-        string="Hallazgo"       
+        string="Hallazgo",
     )
 
     evidence_id = fields.Many2one(
-            string='Evidencia',
-            comodel_name='audit.audit.planning.evidence'
-        )
-    
-    comment = fields.Char(
-        string='Comentario'
-        )
+        string="Evidencia", comodel_name="audit.audit.planning.evidence"
+    )
 
-    evaluation = fields.Char(
-        string='Evaluación'
-        )
+    comment = fields.Char(string="Comentario")
 
-
+    evaluation = fields.Char(string="Evaluación")
 
 
 class Audit(models.Model):
     _inherit = "audit.audit"
 
-    #plan_id = many2one "audit.plan"
-        # Creo que debería quitar el ondelete cascade, porque se podrían borrar actividades y datos de otros modelos independientes, SON MODELOS INDEPENDIENTES
+    # plan_id = many2one "audit.plan"
+    # Creo que debería quitar el ondelete cascade, porque se podrían borrar actividades y datos de otros modelos independientes, SON MODELOS INDEPENDIENTES
 
-    '''
+    """
     audit_plan_id = fields.Many2one(
         string='Programa',
         comodel_name='audit.plan',
     )
-    '''
-    planning_ids = fields.One2many( 
-        comodel_name='audit.audit.planning',
-        inverse_name='audit_audit_id',
-        string='Cronograma')
+    """
+    planning_ids = fields.One2many(
+        comodel_name="audit.audit.planning",
+        inverse_name="audit_audit_id",
+        string="Cronograma",
+    )
 
-    month_training = fields.Selection([
-        ('1', 'Enero'),
-        ('2', 'Febrero'),
-        ('3', 'Marzo'),
-        ('4', 'Abril'),
-        ('5', 'Mayo'),
-        ('6', 'Junio'),
-        ('7', 'Julio'),
-        ('8', 'Agosto'),
-        ('9', 'Septiembre'),
-        ('10', 'Octubre'),
-        ('11', 'Noviembre'),
-        ('12', 'Diciembre'),
-    ],
-        string='Mes elegido',
-        required=False
+    month_training = fields.Selection(
+        [
+            ("1", "Enero"),
+            ("2", "Febrero"),
+            ("3", "Marzo"),
+            ("4", "Abril"),
+            ("5", "Mayo"),
+            ("6", "Junio"),
+            ("7", "Julio"),
+            ("8", "Agosto"),
+            ("9", "Septiembre"),
+            ("10", "Octubre"),
+            ("11", "Noviembre"),
+            ("12", "Diciembre"),
+        ],
+        string="Mes elegido",
+        required=False,
     )
 
     observations = fields.Text(
-    string=u'Observaciones/Alcance',
-    required=False,
+        string="Observaciones/Alcance",
+        required=False,
     )
 
     tyt_sites_related_id = fields.Many2one(
-        'x_sitios',
-        string='Sitio',
-        related='plan_id.sites_id',
+        "x_sitios",
+        string="Sitio",
+        related="plan_id.sites_id",
         store=True,
         readonly=True,
     )
 
     employee_id = fields.Many2one(
-        string='Auditor',
-        comodel_name='hr.employee',
+        string="Auditor",
+        comodel_name="hr.employee",
     )
 
     employee_ids = fields.Many2many(
-        string='Auditados',
-        comodel_name='hr.employee',
+        string="Auditados",
+        comodel_name="hr.employee",
     )
 
     tyt_procedure_id = fields.Many2one(
-        string='Procedimiento',
-        comodel_name='audit.audit.procedure',
+        string="Procedimiento",
+        comodel_name="audit.audit.procedure",
     )
 
     tyt_procedure_description_id = fields.Many2one(
-        string='Procedimiento',
-        comodel_name='audit.plan.schedule.descriptions',
-    )  
+        string="Procedimiento",
+        comodel_name="audit.plan.schedule.descriptions",
+    )
 
     tyt_procedure_activity_id = fields.Many2one(
-        string='Actividad',
-        comodel_name='audit.plan.schedule.activities',
+        string="Actividad",
+        comodel_name="audit.plan.schedule.activities",
         domain="[('description_id', '=', tyt_procedure_description_id)]",
-    )           
-    '''
+    )
+    """
     @api.onchange('tyt_procedure_description_id')
     def _onchange_tyt_procedure_description_id(self):
         if self.tyt_procedure_description_id:
             return {'domain': {'tyt_procedure_activity_id': [('description_id', '=', self.tyt_procedure_description_id.id)]}}
         else:
             return {'domain': {'tyt_procedure_activity_id': []}}
-    '''
+    """
 
-    @api.onchange('tyt_procedure_description_id')
+    @api.onchange("tyt_procedure_description_id")
     def _onchange_tyt_procedure_description_id(self):
         if not self.tyt_procedure_description_id:
             self.tyt_procedure_activity_id = False  # Restablece el campo
         return {
-            'domain': {
-                'tyt_procedure_activity_id': [
-                    ('description_id', '=', self.tyt_procedure_description_id.id)
+            "domain": {
+                "tyt_procedure_activity_id": [
+                    ("description_id", "=", self.tyt_procedure_description_id.id)
                 ]
             }
         }
 
     job_id = fields.Many2one(
-        string='Responsable',
-        comodel_name='hr.job',
+        string="Responsable",
+        comodel_name="hr.job",
     )
 
     audit_date = fields.Date(
-        string='Fecha de Auditoría',
+        string="Fecha de Auditoría",
     )
 
     audited_week = fields.Integer(
-        string='Semana Auditada',
+        string="Semana Auditada",
         default=0,
     )
+
+
+class AuditGenerationForm(models.TransientModel):
+    _name = "audit.generation.form"
+    _description = "Formulario de Generación de Auditoría"
+
+    name = fields.Char(string="Nombre")
+    date = fields.Date(string="Fecha")
+
+    def action_open_audit_form(self):
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Formulario de Auditoría",
+            "view_mode": "form",
+            "res_model": "audit.generation.form",
+            "target": "new",
+        }
