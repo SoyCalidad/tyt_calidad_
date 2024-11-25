0a17652cd85e8bd476c4c0bbec68ffe422e3ca65
Paul Chipana <paulhackingdeveloper@gmail.com>
2024-11-25 14:50:50 -0500

    [ADD] evidence_char + ADJUNTOS button

--- tyt_audit/models/audit_application_form.py
+++ tyt_audit/models/audit_application_form.py
@@ -93,6 +93,7 @@ class AuditApplicationController(http.Controller):
         audit_group = planning_record.audit_audit_id.team_id.name or ""
         finding = planning_record.finding or ""
         norm = ", ".join(planning_record.iso_9001_standards_ids.mapped("combined_name")) or ""
+        evidence_char = planning_record.evidence_char or ""
         evidence = "\n".join(planning_record.evidence_attachment_ids.mapped("name")) or ""
         audit_week = planning_record.audit_audit_id.audited_week or "0"
         audit_date = planning_record.audit_audit_id.audit_date or "Sin Fecha"
@@ -120,6 +121,7 @@ class AuditApplicationController(http.Controller):
                 "audit_group": audit_group or "",
                 "finding": finding or "",
                 "norm": norm or "",
+                "evidence_char": evidence_char or "",
                 "evidence": evidence or "",
                 "audit_week": audit_week or "",
                 "audit_date": audit_date or "",
