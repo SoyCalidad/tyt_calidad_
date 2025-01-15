b00bf8384e129de38c265e322170d0a38704230c
MiguelDev5 <migangmedina@gmail.com>
2025-01-15 17:46:16 -0500

    Add: Kardex by applicant

--- tyt_recruitment/models/requisiton.py
+++ tyt_recruitment/models/requisiton.py
@@ -11,6 +11,11 @@ from urllib.parse import quote
 import logging
 _logger = logging.getLogger(__name__)
 
+class DeparmentDays(models.Model):
+    _inherit = 'hr.department'
+
+    days = fields.Integer( String="Días de capacitación")
+
 class Requisition(models.Model):
     _name = 'tyt_recruitment.requisition'
     _description = 'Requisición'
@@ -251,8 +256,3 @@ class Campaign(models.Model):
                 'default_attendance_id': attendance_list.id
             }
         }
-    
-class DeparmentDays(models.Model):
-    _inherit = 'hr.department'
-
-    days = fields.Integer( String="Días de capacitación")
