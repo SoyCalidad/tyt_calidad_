888db2dfdc57f3291d6f9f05267bfe8b80d19954
Paul Chipana <paulhackingdeveloper@gmail.com>
2025-02-10 15:07:33 -0500

    [FIX] solve WARNING: Same label: State of ... onboarding panel

--- mgmtsystem_employees/models/employee_onboarding.py
+++ mgmtsystem_employees/models/employee_onboarding.py
@@ -19,13 +19,13 @@ class ResCompanyEmployeeOnboarding(models.Model):
     hr_onboarding_state = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), (
         'done', "Done"), ('closed', "Closed")], default='not_done')
     hr_department_onboarding_state = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), (
-        'done', "Done"), ('closed', "Closed")], string="State of the hr department onboarding panel", default='not_done')
+        'done', "Done"), ('closed', "Closed")], default='not_done')
     hr_job_onboarding_state = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), (
-        'done', "Done"), ('closed', "Closed")], string="State of the hr job onboarding panel", default='not_done')
+        'done', "Done"), ('closed', "Closed")], default='not_done')
     hr_employee_onboarding_state = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), (
-        'done', "Done"), ('closed', "Closed")], string="State of the hr employee onboarding panel", default='not_done')
+        'done', "Done"), ('closed', "Closed")], default='not_done')
     # hr_orgchart_onboarding_state = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), (
-    #     'done', "Done"), ('closed', "Closed")], string="State of the hr organization chart onboarding panel", default='not_done')
+    #     'done', "Done"), ('closed', "Closed")], default='not_done')
 
     @api.model
     def action_open_hr_department(self, action_ref=None):
--- soycalidad_improve/models/improve_onboarding.py
+++ soycalidad_improve/models/improve_onboarding.py
@@ -19,7 +19,7 @@ class ResCompany(models.Model):
     improve_onboarding_state = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), (
         'done', "Done"), ('closed', "Closed")], default='not_done')
     improve_plan_change_request = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), (
-        'done', "Done"), ('closed', "Closed")], string="State of the change request invoice onboarding ", default='not_done')
+        'done', "Done"), ('closed', "Closed")], default='not_done')
     improve_plan_onboarding_state = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), (
         'done', "Done"), ('closed', "Closed")], default='not_done')
     improve_improve_onboarding_state = fields.Selection([('not_done', "Not done"), ('just_done', "Just done"), (
