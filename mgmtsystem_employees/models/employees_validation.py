91119b11656c5912768dbaf9abcff57fba6bdc47
Paul Chipana <paulhackingdeveloper@gmail.com>
2025-02-10 17:10:28 -0500

    [FIX] solve WARNING: SELECTION_ADD on mgmtsystem.plan.training

--- mgmtsystem_employees/models/employees_validation.py
+++ mgmtsystem_employees/models/employees_validation.py
@@ -87,15 +87,10 @@ class PlanTraining(models.Model):
 
     state = fields.Selection(
         string=u'Estado',
-        selection=[
-            ('elaborate', 'En elaboración'),
-            ('review', 'En revisión'),
-            ('validate', 'En validación'),
-            ('validate_ok', 'Validado'),
+        selection_add=[
             ('in_process', 'En proceso'),
             ('final', 'Finalizado'),
-            ('caducated', 'Caducado'),
-            ('cancel', 'Obsoleto'),
+            ('caducated', 'Caducado')
         ],
         default='elaborate',
         copy=False,
