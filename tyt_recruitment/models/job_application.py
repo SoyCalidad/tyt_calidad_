84678685feed813e89ee6aa2dc5d6f9069e91f01
Paul Chipana <paulhackingdeveloper@gmail.com>
2025-02-11 12:03:37 -0500

    [ADD] job_application fields on tyt_recruitment.job_application

--- tyt_recruitment/models/job_application.py
+++ tyt_recruitment/models/job_application.py
@@ -90,12 +90,17 @@ class JobApplication(models.Model):
     utility_bill_state = fields.Boolean(string="Estado - Comprobante de domicilio", default=False)
     utility_bill_approved = fields.Boolean(string="Estado de aprobación - Comprobante de domicilio", default=False)
     
-    # health_survey fields fix
+    # health_survey fields fix + job_application fields
     
     health_survey = fields.Binary(string="Encuesta de salud")
     health_survey_filename = fields.Char(string="Nombre del Archivo")
     health_survey_state = fields.Boolean(string="Estado", default=False)
     health_survey_approved = fields.Boolean(string="Estado", default=False)
+
+    job_application = fields.Binary(string="Solicitud de empleo")
+    job_application_filename = fields.Char(string="Nombre del Archivo")
+    job_application_state = fields.Boolean(string="Estado", default=False)
+    job_application_approved = fields.Boolean(string="Estado", default=False)
 
     ###########################
     psychometric = fields.Binary(string="Psicométrico")
