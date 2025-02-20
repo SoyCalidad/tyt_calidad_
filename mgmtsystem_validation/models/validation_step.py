819c6f3e139b2f91e29e424331c42395fd078e95
Paul Chipana <paulhackingdeveloper@gmail.com>
2025-02-20 09:11:02 -0500

    [UPDATE] mgmtsystem.validation : change inherit mail.thread, mail.activity.mixin 1.1

--- mgmtsystem_validation/models/validation_step.py
+++ mgmtsystem_validation/models/validation_step.py
@@ -38,7 +38,7 @@ class ValidationStep(models.Model):
 class Validation(models.Model):
     _name = 'mgmtsystem.validation'
     _description = 'Validación del sistema de gestión'
-    _inherit = ['mgmtsystem.version', 'mail.thread', 'mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin','mgmtsystem.version']
 
     state = fields.Selection(
         string=u'Estado',
