a60b0bf991429e6d577c8eff4ee95c0f0437d572
Dave <fernando.gam@outlook.com>
2025-08-28 18:08:42 -0500

    updated mgmtsystem_audit

--- mgmtsystem_audit/models/audit.py
+++ mgmtsystem_audit/models/audit.py
@@ -410,7 +410,7 @@ class ReportLine(models.Model):
             'type': 'ir.actions.act_window',
             'view_id': attachment_view.id,
             'views': [(attachment_view.id, 'kanban'), (False, 'form')],
-            'view_mode': 'kanban,tree,form',
+            'view_mode': 'kanban,list,form',
             'view_type': 'form',
             'help': _('''<p class="oe_view_nocontent_create">
                         Haga clic para cargar archivos de evidencia
