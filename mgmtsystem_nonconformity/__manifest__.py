37d45b023cd6bbea1eeaef43485e3089044318af
Dave <fernando.gam@outlook.com>
2025-08-27 21:19:59 -0500

    updated mgmtsystem_nonconformity

--- mgmtsystem_nonconformity/__manifest__.py
+++ mgmtsystem_nonconformity/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Management System - Nonconformity",
-    "version": "11.0.1.1.0",
+    "version": "18.0.1.1.0",
     "license": "AGPL-3",
     "category": "Management System",
     "depends": [
