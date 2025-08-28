a2657daf30c4281b7bf5296d0da31ce97630b6dc
Dave <fernando.gam@outlook.com>
2025-08-28 18:01:32 -0500

    updated image of dk

--- document_knowledge/__manifest__.py
+++ document_knowledge/__manifest__.py
@@ -13,6 +13,7 @@
     "website": "https://github.com/OCA/knowledge",
     "depends": ["base"],
     'icon': 'document_knowledge/static/description/icon.svg',
+    'images': ['static/description/icon.svg'],
     "data": [
         "data/ir_module_category.xml",
         "security/document_knowledge_security.xml",
