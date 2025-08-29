8bd8ae3ec5e587611ed80f2e24b95fa559945862
Dave <fernando.gam@outlook.com>
2025-08-28 22:25:08 -0500

    updated mgmtsystem_process_integration

--- mgmtsystem_process_integration/controllers/controllers.py
+++ mgmtsystem_process_integration/controllers/controllers.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import http
-from odoo.addons.web.controllers.main import Home, ensure_db, redirect_with_hash
+from odoo.addons.web.controllers.webclient import WebClient
 from odoo.http import request
 
 
-class AvoidDebug(Home, http.Controller):
+
+class AvoidDebug(WebClient, http.Controller):
 
     @http.route('/web', type='http', auth="none")
     def web_client(self, s_action=None, **kw):
-        ensure_db()
         if 'debug' in kw.keys():
-            return redirect_with_hash('/web')
-        return super(AvoidDebug, self).web_client(s_action=s_action, **kw)
+            kw.pop('debug')
+        return self.web_client(s_action=s_action, **kw)
 
 
